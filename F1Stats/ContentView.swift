9cb780171d6f6d198b3c272fe0e6ea320af53442
Mac Ens <mac@macens.ca>
2021-06-20 09:11:28 -0500

    Initial Commit

---
+++ F1Stats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  F1Stats
+//
+//  Created by Mac Ens on 2021-06-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ F1Stats/F1StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1StatsApp.swift
+//  F1Stats
+//
+//  Created by Mac Ens on 2021-06-20.
+//
+
+import SwiftUI
+
+@main
+struct F1StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
